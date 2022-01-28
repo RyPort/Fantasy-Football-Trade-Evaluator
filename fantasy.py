4c40fc64511e6261795441c81abab35f193291c9
Ryan Porter <58050063+RyPort@users.noreply.github.com>
2022-01-27 23:03:52 -0500

    Viewing points scored after trade works
    CLI for viewing points scored after a trade in FF. There is a bug where the teams will be shown with the wrong players with the output

--- fantasy.py
+++ fantasy.py
@@ -1,68 +1,124 @@
 from espn_api.football import League
+from espn_api.football import Matchup
 
 id = 374100
 year = 2020
 #print(league.teams)
-beat = league.teams[4]
-herb = league.teams[3]
-startWeek = 13
-endWeek = 16
-player1 = "Travis Kelce"
-player2 = "Justin Jefferson"
-player3 = "Darren Waller"
-player4 = "Christian McCaffrey"
+keys = open("keys.txt","r")
 
-player1Sum = 0.0
-player2Sum = 0.0
-player3Sum = 0.0
-player4Sum = 0.0
+my_swid = keys.readline()
+my_swid = my_swid[0:-1]
+my_s2 = keys.readline()
+league = League(league_id=id,year=year,swid=my_swid,espn_s2=my_s2)
+act = league.recent_activity(25,'TRADED')
+
+# players that team1 and team2 traded away respectively
+players_team1 = []
+players_team2 = []
+
+# Gets player score for specified week
+def getPlayerScore(name:str, week:int):
+    # search for player name in given week
+    sb = league.box_scores(week)
+    # search home team roster
+    for score in sb:
+        for player in score.home_lineup:
+            if player.name == name:
+                return player.points
+        for player in score.away_lineup:
+            if player.name == name:
+                return player.points
+    return 0
+def printTrades():
+    num_trade = 1
+    for trade in act:
+        print(str(num_trade) + '. ')
+        num_trade +=1
+        # add team and player names to string list to make it easier to read
+        # get the team names
+        team1 = None
+        team2 = None
+        for item in trade.actions:
+            if team1 == None:
+                team1=item[0]
+            elif team2 == None and team1 != item[0]:
+                team2=item[0]
+        # print players from team 1
+        print(team1.team_name+": ")
+        for item in trade.actions:
+            if item[0] == team1:
+                print(item[2].name+', ')
+        print()
+        print(team2.team_name+': ')
+        for item in trade.actions:
+            if item[0] == team2:
+                print(item[2].name+', ') 
+        
+        print()
+# populates player lists for the teams involved in the trade
+# returns a list with the 2 Team objects    
+def getTradeData():
+    trade_selection = int(input('Select a trade to evaluate: '))
+    # get player names from trade
+    transaction = act[trade_selection-1]
+    team_list = []
+    team1 = None
+    team2 = None
+    for item in transaction.actions:
+        if team1 == None:
+            team1=item[0]
+        elif team2 == None and team1 != item[0]:
+            team2 = item[0]
+        if item[0] == team1:
+            players_team1.append(item[2])
+        else:
+            players_team2.append(item[2])
+    team_list.append(team1)
+    team_list.append(team2)
+    return team_list
+# search team rosters and find week where player first appears
+# param is any player involved in the trade and the team they were traded to
+def getTradeWeek(player_name, player_new_team):
+    for i in range(1,17):
+        for box_score in league.box_scores(i):
+            if box_score.home_team == player_new_team:
+                for player in box_score.home_lineup:
+                    if player.name == player_name:
+                        return i
+            elif box_score.away_team == player_new_team:
+                for player in box_score.away_lineup:
+                    if player.name == player_name:
+                        return i
 
 
-matchup1 = None
-matchup2 = None
+#print(getPlayerScore('Josh Jacobs', 2))
 
-for i in range(startWeek, endWeek):
-    box_scores = league.box_scores(i)
-    for x in box_scores:
-        if(x.home_team == beat or x.away_team == beat):
-            matchup1 = x
-        if(x.home_team == herb or x.away_team == herb):
-            matchup2 = x
-    # have the 2 matchups that the teams were in, get player scores
-    # get team lineups
-    lineup1 = None
-    if(matchup1.home_team == beat):
-        lineup1 = matchup1.home_lineup
-    else:
-        lineup1 = matchup1.away_lineup
-    #get 2nd team lineup
-    lineup2 = None
-    if(matchup2.home_team == herb):
-        lineup2 = matchup2.home_lineup
-    else:
-        lineup2 = matchup2.away_lineup
-    
-    #add players score to the total
-    for y in lineup1:
-        if(y.name == player1):
-            player2Sum += y.points
-        if(y.name == player2):
-            player3Sum += y.points
-    
-    for z in lineup2:
-        if(z.name == player3):
-            player1Sum += z.points
-        if(z.name == player4):
-            player1Sum += z.points
-    
-print(player1, end=" ")
-print(player1Sum)
-print(player2, end=" ")
-print(player2Sum)
-print(player3, end=" ")
-print(player3Sum)
-print(player4, end=" ")
-print(player4Sum)
+printTrades()
+teams = getTradeData()
+team1 = teams[0]
+team2 = teams[1]
+trade_week = getTradeWeek(players_team2[0].name,team1)
+
+# display player totals for team 1
+print(team1.team_name)
+team1_total = 0
+for player in players_team2:
+    sum = 0
+    for i in range(trade_week,17):
+        sum += getPlayerScore(player.name, i)
+    print(player.name+': '+str(sum))
+    team1_total+=sum
+print('The players '+team1.team_name+' received in the trade scored '+str(team1_total)+' points after the trade was completed')
+print()
+print(team2.team_name)
+team2_total = 0
+for player in players_team1:
+    sum = 0
+    for i in range(trade_week,17):
+        sum += getPlayerScore(player.name, i)
+    print(player.name+': '+str(sum))
+    team2_total += sum
+print('The players '+team2.team_name+' received in the trade scored '+str(team2_total)+' points after the trade was completed')
 
 
 
